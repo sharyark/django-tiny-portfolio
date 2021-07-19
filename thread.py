2eec36f30d365d55ea289197aa608af7adbfd241
Shary <ksharyar12345@gmail.com>
2021-07-19 17:21:21 +0000

    pythonThreadingExample

---
+++ thread.py
@@ -0,0 +1,22 @@
+from time import *
+from threading import Thread
+def mybox():
+   while True:
+        print('box is open')
+        sleep(5)
+        print('box is close')
+        sleep(5)
+def myled():
+    while True:
+        print('led is on')
+        sleep(1)
+        print('led is off')
+        sleep(1)
+boxThread  = Thread(target=mybox)
+ledThread = Thread(target=myled)
+boxThread.daemon = True
+ledThread.daemon = True
+boxThread.start()
+ledThread.start()
+while True:
+    pass
